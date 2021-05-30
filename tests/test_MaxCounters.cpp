7574d844750ada54e467176dab2cc49ba36fcdaf
JamesHMcKay <jhmckay93@gmail.com>
2021-05-30 21:58:00 +1200

    updated test

--- tests/test_MaxCounters.cpp
+++ tests/test_MaxCounters.cpp
@@ -11,12 +11,12 @@ TEST(feature, create) {
     std::vector<int> test;
     test.push_back(1);
     test.push_back(2);
-    
+
     std::vector<int> expected;
     expected.push_back(1);
     expected.push_back(1);
     expected.push_back(0);
 
     std::vector<int> result = MaxCounters::solution(3, test);
-    EXPECT_EQ(result[0], expected[0]);
+    EXPECT_EQ(result, expected);
 }
